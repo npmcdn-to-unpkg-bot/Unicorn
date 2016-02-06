ac8a515523c839d729dfe73b1e770a4112f01ead
Peter Deltchev <peter@deltchev.com>
2016-02-05 16:52:45 -0800

    #15: Designed the first database tables.

---
+++ migrations/20160204100938_create_initial_tables.js
@@ -0,0 +1,53 @@
+exports.up = function(knex, Promise) {
+    return Promise.all([
+        knex.schema.createTable('users', function (table) {
+            table.uuid('id').primary();
+            table.string('username');
+            table.timestamps();
+        }),
+
+        knex.schema.createTable('comic_user', function (table) {
+            table.uuid('user_id')
+                .references('id').inTable('users');
+            table.uuid('comic_id')
+                .references('id').inTable('comics');
+            table.boolean('is_owner');
+            table.timestamps();
+            table.primary(['user_id', 'comic_id']);
+        }),
+
+        knex.schema.createTable('comics', function (table) {
+            table.uuid('id').primary();
+            table.string('title');
+            table.timestamps();
+        }),
+
+        knex.schema.createTable('comic_panels', function (table) {
+            table.uuid('id').primary();
+            table.uuid('comic_id')
+                .references('id').inTable('comics');
+            table.integer('position').unsigned();
+            table.timestamps();
+        }),
+
+        knex.schema.createTable('speech_bubbles', function (table) {
+            table.uuid('id').primary();
+            table.uuid('comic_panel_id')
+                .references('id').inTable('comic_panels');
+            table.text('text');
+            table.integer('position_x').unsigned();
+            table.integer('position_y').unsigned();
+            table.timestamps();
+        })
+    ]);
+};
+
+exports.down = function(knex, Promise) {
+    return Promise.all([
+        knex.schema.dropTable('speech_bubbles'),
+        knex.schema.dropTable('comic_user'),
+        knex.schema.dropTable('comic_panels'),
+        knex.schema.dropTable('comics'),
+        knex.schema.dropTable('users')
+    ]);
+};
