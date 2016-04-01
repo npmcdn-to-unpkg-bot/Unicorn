111a15c20a6c8072343c68273be6a4d95de3577b
Peter Deltchev <peter@deltchev.com>
2016-04-01 15:55:50 -0700

    Fixed broken favourites.

---
+++ migrations/20160401224407_fix_saved_comics.js
@@ -0,0 +1,18 @@
+
+exports.up = function(knex, Promise) {
+    return Promise.all([
+        knex.schema
+            .table('saved_comics', function (table) {
+                table.timestamps();
+            })
+    ]);
+};
+
+exports.down = function(knex, Promise) {
+    return Promise.all([
+        knex.schema
+            .table('saved_comics', function (table) {
+                table.dropTimestamps();
+            })
+    ]);
+};
